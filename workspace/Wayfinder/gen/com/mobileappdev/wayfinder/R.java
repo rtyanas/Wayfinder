b1aa0cc7131b70b274bda7eb6c78bf6d073a29ff
rtyanas <rtyanas2000@yahoo.com>
2013-11-21 17:10:26 -0500

    Update resources

--- workspace/Wayfinder/gen/com/mobileappdev/wayfinder/R.java
+++ workspace/Wayfinder/gen/com/mobileappdev/wayfinder/R.java
@@ -22,18 +22,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int allison_road_classroom_building_3878=0x7f020000;
-        public static final int arc_building=0x7f020001;
-        public static final int arc_icon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int ic_menu_refresh=0x7f020004;
-        public static final int ic_menu_save=0x7f020005;
-        public static final int livingston_student_center_4160=0x7f020006;
-        public static final int menu_icon=0x7f020007;
-        public static final int sc_bush_icon=0x7f020008;
-        public static final int sc_douglas_icon=0x7f020009;
-        public static final int sc_livingston_icon=0x7f02000a;
-        public static final int student_activities_center_3123=0x7f02000b;
-        public static final int university_centerat_easton_avenue_3154=0x7f02000c;
+        public static final int allison_road_classroom_building_3878_sm=0x7f020001;
+        public static final int arc_building=0x7f020002;
+        public static final int arc_icon=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_menu_refresh=0x7f020005;
+        public static final int ic_menu_save=0x7f020006;
+        public static final int livingston_student_center_4160=0x7f020007;
+        public static final int menu_icon=0x7f020008;
+        public static final int sc_bush_icon=0x7f020009;
+        public static final int sc_douglas_icon=0x7f02000a;
+        public static final int sc_livingston_icon=0x7f02000b;
+        public static final int student_activities_center_3123=0x7f02000c;
+        public static final int university_centerat_easton_avenue_3154=0x7f02000d;
     }
     public static final class id {
         public static final int action_add=0x7f090006;
