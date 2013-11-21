8895500da5274750f614a04944ef6251adc1b184
rtyanas <rtyanas2000@yahoo.com>
2013-11-21 17:04:59 -0500

    Move BuildingNameIcon to its own class Add getBuildings array

--- workspace/Wayfinder/src/com/mobileappdev/wayfinder/Buildings.java
+++ workspace/Wayfinder/src/com/mobileappdev/wayfinder/Buildings.java
@@ -42,44 +42,7 @@ public class Buildings {
 		return bName;
 	}
 
-
-    
-    public class BuildingNameIcon {
-    	String buildingName;
-    	int drawable;
-    	int hashCode;
-    	
-    	public BuildingNameIcon (String buildN_in, int draw_in) {
-        	buildingName = buildN_in;
-        	drawable = draw_in;
-        	hashCode = buildingName.hashCode();
-    	}
-
-		public String getBuildingName() {
-			return buildingName;
-		}
-
-		public void setBuildingName(String buildingName) {
-			this.buildingName = buildingName;
-		}
-
-		public int getDrawable() {
-			return drawable;
-		}
-
-		public void setDrawable(int drawable) {
-			this.drawable = drawable;
-		}
-
-		public int getHashCode() {
-			return hashCode;
-		}
-
-		public void setHashCode(int hashCode) {
-			this.hashCode = hashCode;
-		}
-    	
-		
-    	
-    }
+	public ArrayList<BuildingNameIcon> getBuildings() {
+		return (ArrayList<BuildingNameIcon>)buildings.clone();
+	}
 }
