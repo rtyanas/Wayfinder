cd9af6542ef02d56467668fa72240c3303bc7924
rtyanas <rtyanas2000@yahoo.com>
2013-12-01 20:09:13 -0500

    add floor plan add class OneIconDef

--- workspace/Wayfinder/src/com/mobileappdev/wayfinder/BuildingNameIcon.java
+++ workspace/Wayfinder/src/com/mobileappdev/wayfinder/BuildingNameIcon.java
@@ -1,19 +1,31 @@
 package com.mobileappdev.wayfinder;
 
+import java.util.Vector;
+
 public class BuildingNameIcon {
 	String buildingName;
 	int drawable;
 	int hashCode;
+	Vector<OneIconDef> floorPlan = new Vector<OneIconDef> ();
 	
 	public BuildingNameIcon () {
 		buildingName = "";
 		drawable = 0;		
+		floorPlan = new Vector<OneIconDef>();
 	}
 	
 	public BuildingNameIcon (String buildN_in, int draw_in) {
     	buildingName = buildN_in;
     	drawable = draw_in;
     	hashCode = buildingName.hashCode();
+	}
+	
+	public Vector<OneIconDef> getFloorPlan() {
+		return floorPlan;
+	}
+
+	public void setFloorPlan(Vector<OneIconDef> floorPlan_in) {
+		floorPlan = floorPlan_in;
 	}
 
 	public String getBuildingName() {
@@ -41,5 +53,41 @@ public class BuildingNameIcon {
 	}
 	
 	
-	
+	class OneIconDef {
+		int row, column;
+		int drawable;
+		
+		OneIconDef(int row_in, int column_in, int drawable_in) {
+			row = row_in;
+			column = column_in;
+			drawable = drawable_in;
+		}
+
+		public int getRow() {
+			return row;
+		}
+
+		public void setRow(int row) {
+			this.row = row;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		public void setColumn(int column) {
+			this.column = column;
+		}
+
+		public int getDrawable() {
+			return drawable;
+		}
+
+		public void setDrawable(int drawable) {
+			this.drawable = drawable;
+		}
+		
+		
+	}
+
 }
